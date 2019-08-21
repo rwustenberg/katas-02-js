b1ccd0e529a51153d964f02ccd87921db94fc3da
ruthw <rwustenberg@gmail.com>
2019-08-21 08:41:51 -0500

    math katas

--- Math-katas.js
+++ Math-katas.js
@@ -9,7 +9,39 @@ console.log(roundNum(3.14))
 console.log(roundNum(15.9))
 
 //020202
-let productsPrice = (prodPrice) => {
-	return Math.round(prodPrice.price)
+
+let prodPrice = (priceArr) => {
+	return priceArr.map((pr,ind)=>{
+		let rp = Math.round(pr.price)
+		pr.price = rp
+		return pr
+	})
 }
-console.log(productsPrice(products))
+console.log(prodPrice(products))
+
+
+let newArr = products.map((element)=> {
+	 return (element.price)
+})
+ let roundedNums = (prodPrice) => {Math.round(prodPrice)
+}
+
+//020203
+let numFunction = (num,x) => {
+	return num.toFixed(x)
+}
+	console.log(numFunction(3.148,2))
+
+	//020204
+	let powerVar = (num,pow) => {
+		return Math.pow(num,pow)
+	}
+		console.log(powerVar(2,3))
+
+//020205
+	let randomNum = (x) => {
+		return Math.random()
+	}
+		console.log(randomNum() * 10)
+
+//020206
